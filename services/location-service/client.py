76b6eff8ff21f8db5e003276c835b363731638e3
laurijssen <lauserve@hotmail.com>
2020-11-21 07:38:21 +0000

    retrieve locationservice ip by hostname

--- services/location-service/client.py
+++ services/location-service/client.py
@@ -1,14 +1,16 @@
 from __future__ import print_function
 import logging
+import socket
 
 import grpc
 
 from proto.service_pb2 import EchoRequest
 from proto.service_pb2_grpc import EchoStub
 
+ip = [i[4][0] for i in socket.getaddrinfo('kubemaster', None)][0]
 
 def run():
-    with grpc.insecure_channel('192.168.56.3:30700') as channel:
+    with grpc.insecure_channel(f'{ip}:30700') as channel:
         stub = EchoStub(channel)
         response = stub.Reply(EchoRequest(message='Hoppa'))
     print("Echo client received: " + response.message)
