0c79ba588a4e53914828dcc14d376d5dbb83261d
laurijssen <lauserve@hotmail.com>
2020-11-21 07:57:30 +0000

    added counter

--- services/location-service/location_grpc.py
+++ services/location-service/location_grpc.py
@@ -1,7 +1,11 @@
 import proto.service_pb2_grpc
 import proto.service_pb2
 
-class Echoer(proto.service_pb2_grpc.EchoServicer):
+class Echoer(proto.service_pb2_grpc.EchoServicer):    
+
+    i = 0
+
     def Reply(self, request, context):
-        reply = f'\nYou said: {request.message}' * 10
+        reply = f'\nYou said: {request.message} {self.i}' * 10
+        self.i += 1
         return proto.service_pb2.EchoReply(message=reply)
