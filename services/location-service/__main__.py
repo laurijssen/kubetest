d44731abd1218cee16b60eebf30365c4e09b233a
laurijssen <lauserve@hotmail.com>
2020-11-21 07:25:55 +0000

    working python grpc service

--- services/location-service/__main__.py
+++ services/location-service/__main__.py
@@ -1,4 +1,19 @@
-from location_service import Server
+from concurrent import futures
+import grpc
+
+from proto.service_pb2_grpc import add_EchoServicer_to_server
+from location_grpc import Echoer
+
+class Server:
+
+    @staticmethod
+    def run():
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        add_EchoServicer_to_server(Echoer(), server)
+        server.add_insecure_port('[::]:8081')
+        server.start()
+        server.wait_for_termination()
+
 
 if __name__ == '__main__':
     print("start server")
--- services/location-service/location_grpc.py
+++ services/location-service/location_grpc.py
@@ -3,5 +3,5 @@ import proto.service_pb2
 
 class Echoer(proto.service_pb2_grpc.EchoServicer):
     def Reply(self, request, context):
-        reply = f'You said: {request.message}\n' * 4
+        reply = f'\nYou said: {request.message}' * 10
         return proto.service_pb2.EchoReply(message=reply)
